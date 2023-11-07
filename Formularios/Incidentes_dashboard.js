15311e1824e6602a9b55cb06e0a79004b7b6c2bb
Luis Nicolas Arevalo Chiquiza <nicolasarevalochiquiza@gmail.com>
2023-11-07 18:58:05 -0500

    Inicio de dashboard inspeccion

--- Formularios/Incidentes_dashboard.js
+++ Formularios/Incidentes_dashboard.js
@@ -1,48 +1,77 @@
-.bi {
-  display: inline-block;
-  width: 1rem;
-  height: 1rem;
-}
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
 
-/*
- * Sidebar
- */
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
 
-@media (min-width: 768px) {
-  .sidebar .offcanvas-lg {
-    position: -webkit-sticky;
-    position: sticky;
-    top: 48px;
-  }
-  .navbar-search {
-    display: block;
-  }
-}
+      form.classList.add('was-validated')
+    }, false)
+  });
 
-.sidebar .nav-link {
-  font-size: .875rem;
-  font-weight: 500;
-}
+  /* Funcion para el boton de filtrado */
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    const tablaTareas = document.getElementById("Tabla_incidentes");
+    const dropdown = document.querySelector(".dropdown");
 
-.sidebar .nav-link.active {
-  color: #2470dc;
-}
+    dropdown.addEventListener("click", function (event) {
+      if (event.target.classList.contains("dropdown-item")) {
+        const selectedProject = event.target.textContent;
+        filterTable(selectedProject);
+      }
+    });
 
-.sidebar-heading {
-  font-size: .75rem;
-}
+    
+    function filterTable(selectedProject) {
+      const rows = tablaTareas.querySelectorAll("tbody tr");
+      rows.forEach(function (row) {
+        const proyecto = row.cells[0].textContent;
+        if (
+          selectedProject === "Todos los proyectos" ||
+          proyecto === selectedProject
+        ) {
+          row.style.display = "";
+        } else {
+          row.style.display = "none";
+        }
+      });
+    }
+  });
 
-/*
- * Navbar
- */
+  /* opcion para que se vea la pestaña de seguimiento del heder del modal*/
+  const seguimientoDiv = document.querySelector('#Seguimiento');
+  const detallesDiv = document.querySelector('#Detalles_incidente');
+  // Add click event listeners to the Seguimiento and Detalles buttons
+document.querySelector('#SeguimientoButton').addEventListener('click', () => {
+  seguimientoDiv.style.display = seguimientoDiv.style.display === 'none' ? 'block' : 'none';
+  detallesDiv.style.display = 'none';
+  seguimientoDiv.dataset.bsActive = true;
+});
 
-.navbar-brand {
-  padding-top: .75rem;
-  padding-bottom: .75rem;
-  background-color: rgba(0, 0, 0, .25);
-  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .25);
-}
+document.querySelector('#DetallesButton').addEventListener('click', () => {
+  detallesDiv.style.display = detallesDiv.style.display === 'none' ? 'block' : 'none';
+  seguimientoDiv.style.display = 'none';
+});
 
-.navbar .form-control {
-  padding: .75rem 1rem;
-}
+  /* opcion para que se vea la pestaña de seguimiento del menu desplegable*/
+  const seguimiento_desplegable_div = document.querySelector('#Seguimiento');
+  const detalle_desplegable_div = document.querySelector('#Detalles_incidente');
+  // Add click event listeners to the Seguimiento and Detalles buttons
+document.querySelector('.btn-desplegable-seguimiento').addEventListener('click', () => {
+  seguimiento_desplegable_div.style.display = seguimiento_desplegable_div.style.display === 'none' ? 'block' : 'none';
+  detalle_desplegable_div.style.display = 'none';
+});
+
+document.querySelector('.btn-desplegable-detalles').addEventListener('click', () => {
+  detalle_desplegable_div.style.display = detalle_desplegable_div.style.display === 'none' ? 'block' : 'none';
+  seguimiento_desplegable_div.style.display = 'none';
+});
+})();
