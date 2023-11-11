66b3fcf4fe0a35a4ef1c8f14e81bd1596204ea0a
Luis Nicolas Arevalo Chiquiza <nicolasarevalochiquiza@gmail.com>
2023-11-11 15:42:26 -0500

    na

--- Formularios/crear_fase.php
+++ Formularios/crear_fase.php
@@ -163,7 +163,7 @@
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h5 class="modal-title" id="confirmModalLabel">Confirmar envío</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         <span aria-hidden="true">&times;</span>
                                     </button>
                                 </div>
