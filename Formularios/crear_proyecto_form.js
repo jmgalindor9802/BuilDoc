2570586f70c4f931b7014616497e8e7752625e64
JulianAmado <j.amadosena@gmail.com>
2023-11-25 21:07:45 -0500

    Ajustes formulario proyecto y usuario

--- Formularios/crear_proyecto_form.js
+++ Formularios/crear_proyecto_form.js
@@ -16,3 +16,37 @@
       }, false)
     })
   })()
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const municipiosPorDepartamento = {
+        "Amazonas": ["Leticia", "Tabatinga", "Puerto Nariño"],
+        "Antioquia": ["Medellín", "Bello", "Envigado"],
+    };
+
+    const departamentoSelect = document.getElementById("departamento");
+    const municipioSelect = document.getElementById("municipio");
+
+    // Función para cargar municipios según el departamento seleccionado
+    function actualizarMunicipios() {
+        const selectedDepartamento = departamentoSelect.value;
+        municipioSelect.innerHTML = ""; // Limpiar opciones
+
+        // Si se selecciona un departamento válido, cargar los municipios correspondientes
+        if (municipiosPorDepartamento.hasOwnProperty(selectedDepartamento)) {
+            const municipios = municipiosPorDepartamento[selectedDepartamento];
+            municipios.forEach(municipio => {
+                const option = document.createElement("option");
+                option.text = municipio;
+                option.value = municipio;
+                municipioSelect.appendChild(option);
+            });
+        } else {
+            const option = document.createElement("option");
+            option.text = "Selecciona un departamento válido";
+            municipioSelect.appendChild(option);
+        }
+    }
+
+    // Escuchar cambios en el select de departamento para actualizar los municipios
+    departamentoSelect.addEventListener("change", actualizarMunicipios);
+});
