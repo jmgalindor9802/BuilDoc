11c4983c4665030804e7c4141c6b310e5ae76e7c
JavierCoC <104465195+JavierCoC@users.noreply.github.com>
2023-11-29 07:16:08 -0500

    php

--- Formularios/Inspecciones_dashboar.php
+++ Formularios/Inspecciones_dashboar.php
@@ -326,20 +326,21 @@
       </div>
     </div>
   </div>
-  <script src="Incidentes_dashboard.js"></script>
+  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-  <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
   <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+  <script src="Incidentes_dashboard.js"></script>
   <script type="text/javascript">
-
     $(document).ready(function(){
         $('#Tabla_incidentes').DataTable();
     });
-
   </script>
+
   
 </body>
 
